51d3ccc71ccdd7768220a95ce2d0b1c744eacb50
Raghu Kumar <raghu.kumar@cognistreamer.com>
2018-04-13 22:49:19 +0530

    assets updates

--- App.js
+++ App.js
@@ -20,7 +20,7 @@ export default class App extends Component {
 
     _loadAssetsAsync = async () => {
         const fontAssets = this._cacheFonts([
-            Ionicons.font,
+            ...Ionicons.font,
             {'Opensans': require('./assets/fonts/Opensans.ttf') },
             {'Titillium': require('./assets/fonts/Titillium.ttf') }
         ]);
@@ -32,11 +32,11 @@ export default class App extends Component {
             require('./assets/images/pin.png'),
             require('./assets/images/splash.png'),
             require('./assets/images/statusbar.png'),
-            require('./assets/steps/relaxation.png'),
-            require('./assets/steps/concentration.png'),
-            require('./assets/steps/contemplation.png'),
-            require('./assets/steps/realisation.png'),
-            require('./assets/steps/meditation.png')
+            require('./assets/images/steps/relaxation.png'),
+            require('./assets/images/steps/concentration.png'),
+            require('./assets/images/steps/contemplation.png'),
+            require('./assets/images/steps/realisation.png'),
+            require('./assets/images/steps/meditation.png')
         ]);
 
         await Promise.all([imageAssets, ...fontAssets]);
